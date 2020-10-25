0a9b805faa1a67d72ca83213eb4d1d78e7ce7af1
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-10-25 23:57:14 +0800

    NewsController

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[\App\Http\Controllers\HomeController::class,'index'])->name('home.index');//a
 Route::get('/about',[\App\Http\Controllers\AboutController::class,'index'])->name('about.index');//b
-Route::get('/news',[\App\Http\Controllers\NewsController::class,'index'])->name('news.index');
+Route::get('/news',[\App\Http\Controllers\NewsController::class,'index'])->name('news.index');//c
 
 
 
