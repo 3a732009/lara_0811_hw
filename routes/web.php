035c68eadc1dcfc365b4902dd1be4e9587bc883e
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-10-25 23:50:41 +0800

    HomeController

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[\App\Http\Controllers\HomeController::class,'index'])->name('home.index');//a
+Route::get('/about',[\App\Http\Controllers\AboutController::class,'index'])->name('about.index');
+Route::get('/news',[\App\Http\Controllers\NewsController::class,'index'])->name('news.index');
+
+
+
+
